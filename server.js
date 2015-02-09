b4bce0222e3bde8cd7e8918a5e2e90955841ab79
Lalit Kapoor <lalitkapoor@gmail.com>
2015-02-09 13:13:31 -0500

    output port details when starting server

--- server.js
+++ server.js
@@ -6,4 +6,11 @@ var http = require('http')
 var config = require('./config')
 var app = require('./app')
 
-http.createServer(app).listen(config.port)
+http.createServer(app).listen(config.port, function(error) {
+  if (error) {
+    console.log('could NOT listen on port %s', config.port)
+    console.error(error)
+    return process.exit(1)
+  }
+  console.log('listening on port %s', config.port)
+})
