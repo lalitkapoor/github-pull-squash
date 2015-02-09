8e61c6885c7cd2ef30b75f73e16a7172c542901f
Lalit Kapoor <lalitkapoor@gmail.com>
2015-02-09 13:11:37 -0500

    clean up

--- routes/auth.js
+++ routes/auth.js
@@ -19,11 +19,11 @@ var github = new GitHubApi({
 })
 
 // move this client side
-router.get('/github', function (req, res) {
+router.get('/github', function(req, res) {
   var scope = ['user', 'repo', 'write:repo_hook']
   var githubUrl = 'https://github.com/login/oauth/authorize?'
 
-  crypto.randomBytesAsync(48).then(function (buf) {
+  crypto.randomBytesAsync(48).then(function(buf) {
     // we should be saving the state to a datastore for security
     var state = buf.toString('hex')
 
@@ -40,7 +40,7 @@ router.get('/github', function (req, res) {
   })
 })
 
-router.get('/github/callback', function (req, res) {
+router.get('/github/callback', function(req, res) {
   var githubUrl = 'https://github.com/login/oauth/access_token'
 
   // we should be checking this, but ignore for now, store in a datastore
@@ -55,20 +55,20 @@ router.get('/github/callback', function (req, res) {
   })
   .set('Content-Type', 'application/json')
   .set('Accept', 'application/json')
-  .end(function (error, response){
-    if (error) return res.send(500, 'an error occurred')
+  .end(function(error, response) {
+    if (error) return res.status(500).send('an error occurred')
 
     var oauth = response.body
     var token = oauth.access_token
 
     github.authenticate({type: 'oauth', token: token})
-    github.user.get({}, function (err, user) {
-      if (error) return res.send(500, 'an error occurred talking to github')
-      if (!user.id) return res.send(500, 'an error occurred getting user from github')
+    github.user.get({}, function(err, user) {
+      if (error) return res.status(500).send('an error occurred talking to github')
+      if (!user.id) return res.status(500).send('an error occurred getting user from github')
 
       var data = _.extend({}, user, {oauth: oauth})
-      redis.HMSET('user:' + user.id, data, function (error) {
-        if (error) return res.send(500, 'an error occurred talking to our database')
+      redis.HMSET('user:' + user.id, data, function(error) {
+        if (error) return res.status(500).send('an error occurred talking to our database')
         // set user cookie, because they are now authenticated
         req.session.github = {user: user, oauth: oauth, token: token}
         return res.redirect('/')
